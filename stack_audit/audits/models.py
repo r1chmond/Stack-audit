3a85a8ad4535d0739c768eba508e2314ac5ea36f
KwanYuiChiu <k21003778@kcl.ac.uk>
2023-10-21 14:59:31 +0100

    Initial model

--- stack_audit/audits/models.py
+++ stack_audit/audits/models.py
@@ -1,3 +1,18 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+
 # Create your models here.
+class Evaluation(models.Model):
+    score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    response = models.TextField()
+
+class SmartContract(models.Model):
+    contract_address = models.TextField()
+    contract_name = models.TextField()
+    evalutation = models.ForeignKey(Evaluation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"SmartContract({self.contract_name}, {self.contract_address})"
+
+
