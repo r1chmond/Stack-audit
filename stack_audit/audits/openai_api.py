9d712c58aa94974ac7269921d31b1b461b6304c3
KwanYuiChiu <k21003778@kcl.ac.uk>
2023-10-21 16:11:36 +0100

    Completed View function

---
+++ stack_audit/audits/openai_api.py
@@ -0,0 +1,5 @@
+from audits.forms import SmartContractForm
+
+
+def get_response(form: SmartContractForm)-> str:
+    return ""
--- stack_audit/audits/views.py
+++ stack_audit/audits/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-
+from audits import openai_api
 from audits.forms import SmartContractForm
 
 
 # Create your views here.
 def home(request):
     form = SmartContractForm()
-    if request.method == "post":
-        pass
-    return render(request, 'home.html', context={"form": form})
+    context = {}
+    if request.method == "POST":
+        form = SmartContractForm(request.POST)
+        response = openai_api.get_response(form)
+        context['response'] = response
+
+    context["form"] = form
+    return render(request, 'home.html', context=context)
